74602a09eb9534842e03bf5e37f1db780ccf0392
Marsh-bp <bharathpranesh2004@gmail.com>
2025-03-29 11:15:05 +0530

    first commit

---
+++ fleet_management_system/src/models/nav_graph.py
@@ -0,0 +1,39 @@
+class NavGraph:
+    def __init__(self, graph):
+        self.vertices = self.parse_vertices(graph["vertices"])
+        self.lanes = self.parse_lanes(graph["lanes"])
+        self.adj = self.build_adjacency_list()
+    def parse_vertices(self, vertices_raw):
+        vertices = {}
+        for idx, vertex in enumerate(vertices_raw):
+            x, y, properties = vertex
+            vertices[idx] = {
+                "index": idx,
+                "pos": (x * 50, -y * 50),
+                "name": properties.get("name",""),
+                "is_charger": properties.get("is_charger", False)
+            }
+        return vertices
+    def parse_lanes(self, lanes_raw):
+        return [(lane[0], lane[1]) for lane in lanes_raw]
+    
+    def build_adjacency_list(self):
+        adj = {idx: [] for idx in self.vertices}
+        for a,b in self.lanes:
+            adj[a].append(b)
+            adj[b].append(a)
+        return adj
+    
+    def find_path(self, start, goal):
+        from collections import deque
+        queue = deque([(start, [start])])
+        visited = {start}
+        while queue:
+            current, path = queue.popleft()
+            if current == goal:
+                return path
+            for neighbor in self.adj[current]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append((neighbor, path + [neighbor]))
+        return None
